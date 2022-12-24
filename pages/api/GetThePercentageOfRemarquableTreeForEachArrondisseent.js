d2b3dbf9720056d1a11c56f9aa4b5508ffb6965f
MarwenLabidi <marwenlabidi.dev@gmail.com>
2022-12-24 12:40:30 +0100

    🎉 i finish working on the GetThePercentageOfRemarquableTreeForEachArrondisseent serverless function

--- pages/api/GetThePercentageOfRemarquableTreeForEachArrondisseent.js
+++ pages/api/GetThePercentageOfRemarquableTreeForEachArrondisseent.js
@@ -1,15 +1,17 @@
 
 //NOTE : write a function to filter all the trees by arrondissement(arrays of ech arondisment have his trees) and count the number(percentage) of remarquabale trees for each arrondissement
 import { fetchArbreRemarquableParis } from "../../lib/fetchTreeDataTenItemPerPage";
-import { getArrondissement } from "../../lib/filterData";
+import { getArrondissement ,getTreesByLocation} from "../../lib/filterData";
 
 export default async function handler(req, res) {
         try {
                 const Trees = await fetchArbreRemarquableParis();
                 const arrondissement = getArrondissement(Trees);
+                const treesByLocation = getTreesByLocation(arrondissement, Trees);
+
 
                 if (Trees.length !== 0) {
-                        res.json(arrondissement);
+                        res.json(treesByLocation);
                 } else {
                         res.json({
                                 message: `page could not be found`,
