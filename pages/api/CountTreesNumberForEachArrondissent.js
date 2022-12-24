04bef89d818264153aa569f77e69b59c912d1693
MarwenLabidi <marwenlabidi.dev@gmail.com>
2022-12-24 10:32:03 +0100

    🚧 Work in progress : im working on the countTreesNumberForEachArrondissement serverless function

--- pages/api/CountTreesNumberForEachArrondissent.js
+++ pages/api/CountTreesNumberForEachArrondissent.js
@@ -1,6 +1,10 @@
 
-//NOTE : there is 20 arrondissements in Paris so loop through the data and count the number of trees for each arrondissement
+//NOTE : there is 20 arrondissements in Paris
+//NOTE : write a function to filter all the trees by arrondissement(arrays of ech arondisment have his trees) and count the number(percentage) of trees for each arrondissement
+
 export default function handler(req, res) {
-	res.status(200).json({ name: 'CountTreesNumberForEachArrondissent' })
+	//write a function to get all arrondissements from the data
+	// write a function to delete duplicate value
+	// write a function to count the number of trees for each arrondissement	
       }
       
--- pages/api/GetThePercentageOfRemarquableTreeForEachArrondisseent.js
+++ pages/api/GetThePercentageOfRemarquableTreeForEachArrondisseent.js
@@ -1,5 +1,5 @@
 
-//NOTE : write a function to filter all the trees by arrondissement(arrays of ech arondisment have his trees) and count the number(percentage) of trees for each arrondissement
+//NOTE : write a function to filter all the trees by arrondissement(arrays of ech arondisment have his trees) and count the number(percentage) of remarquabale trees for each arrondissement
 export default function handler(req, res) {
   res.status(200).json({ name: 'GetThePercentageOfRemarquableTreeForEachArrondisseent' })
 }
