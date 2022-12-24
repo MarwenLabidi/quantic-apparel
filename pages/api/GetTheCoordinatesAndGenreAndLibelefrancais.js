ab95883ff3e11059b168d581f5179fb7b2d551b7
MarwenLabidi <marwenlabidi.dev@gmail.com>
2022-12-24 11:22:22 +0100

    🎉 i finish working on GetCoordinatesAndGenreAndLibellefrancais serverless function 🎉

--- pages/api/GetTheCoordinatesAndGenreAndLibelefrancais.js
+++ pages/api/GetTheCoordinatesAndGenreAndLibelefrancais.js
@@ -1,5 +1,20 @@
-export default function handler(req, res) {
-        res.status(200).json({
-                name: "GetTheCoordinatesAndGenreAndLibelefrancais",
-        });
+import { fetchArbreRemarquableParis } from "../../lib/fetchTreeDataTenItemPerPage";
+import { getDataCoordinateGenreLibellefrancais } from "../../lib/filterData";
+
+export default async function handler(req, res) {
+        try {
+                const Trees = await fetchArbreRemarquableParis();
+                const filtredAtt = getDataCoordinateGenreLibellefrancais(Trees);
+
+                if (Trees.length !== 0) {
+                        res.json(filtredAtt);
+                } else {
+                        res.json({
+                                message: `page could not be found`,
+                        });
+                }
+        } catch (error) {
+                res.status(500);
+                res.json({ message: "Something went wrong", error });
+        }
 }
