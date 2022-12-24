67c69944e746be40a3192fcfda1a971c4bc27b33
MarwenLabidi <marwenlabidi.dev@gmail.com>
2022-12-24 23:38:53 +0100

    🚧 Work in progress : i start  working on les arbres remarquables page

---
+++ components/table/column.js
@@ -0,0 +1,10 @@
+export const COLUMNN = [
+        { Header: "idbase",accessor:"idbase" },
+        { Header: "libellefrancais",accessor:"libellefrancais" },
+        { Header: "genre" ,accessor:"genre" },
+        { Header: "espece",accessor:"espece" },
+        { Header: "circonferenceencm",accessor:"circonferenceencm" },
+        { Header: "hauteurenm",accessor:"hauteurenm" },
+        { Header: "dateplantation",accessor:"dateplantation" },
+        { Header: "adresse",accessor:"adresse" },
+];
