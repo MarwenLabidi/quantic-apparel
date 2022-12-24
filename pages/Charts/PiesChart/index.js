abbf58d70a7b00c4429114a7f4bf70f84b11d54f
MarwenLabidi <marwenlabidi.dev@gmail.com>
2022-12-24 22:41:35 +0100

    🚧 Work in progress : im working on the pies graph

--- pages/Charts/PiesChart/index.js
+++ pages/Charts/PiesChart/index.js
@@ -3,28 +3,24 @@ import { ResponsivePie } from "@nivo/pie"
 import {piesChart}from"./index.module.css"
 import useSWR from "swr";
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
-const pieData = [
-  {
-    id: "java",
-    label: "java",
-    value: 100,
-    color: "hsl(90, 70%, 50%)"
-  },]
+
 const PiesChart = () => {
   const { data, error, isLoading } = useSWR(URL, fetcher);
 
   return (
     <div className={piesChart}>
    {
-    (data)&&(data.map((pieData)=>(
+    (data)&&(data.map((pieData,index)=>(
       <ResponsivePie
       data={pieData}
-      // margin={{ top: 240, right: 280, bottom: 280, left: 280 }}
+      key={index}
+      // margin={{ top: 340, right: 280, bottom: 280, left: 280 }}
       innerRadius={0.5}
       padAngle={0.7}
       cornerRadius={3}
       activeOuterRadiusOffset={8}
       borderWidth={1}
+      animate={false}
       borderColor={{ from: "color", modifiers: [["darker", 0.2]] }}
       arcLinkLabelsSkipAngle={10}
       arcLinkLabelsTextColor="#333333"
@@ -32,6 +28,8 @@ const PiesChart = () => {
       arcLinkLabelsColor={{ from: "color" }}
       arcLabelsSkipAngle={10}
       arcLabelsTextColor={{ from: "color", modifiers: [["darker", 2]] }}
+      height={250}
+      width={250}
     />
 
     )))
