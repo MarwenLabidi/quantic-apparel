03208935b8a7f8b2077a0faca0e1bd0c55670a31
MarwenLabidi <marwenlabidi.dev@gmail.com>
2022-12-24 09:42:13 +0100

    🚧 Work in progress : im working on the GetLesArbresRemarquablesData serveless function

--- pages/api/GetLesArbresRemarquablesData.js
+++ pages/api/GetLesArbresRemarquablesData.js
@@ -1,5 +1,30 @@
 
 //NOTE : use start query parameter to get the next page of data (pagination) 10 items per page
-export default function handler(req, res) {
-  res.status(200).json({ name: 'GetLesArbresRemarquablesData' })
+// http://localhost:3000/api/GetLesArbresRemarquablesData?rows=10&start=1
+
+import { fetchArbreRemarquableParis } from "../../lib/fetchTreeDataTenItemPerPage";
+
+
+export default async function handler(req, res) {
+  const {rows, start } = req.query;
+
+  try {
+    if (rows) {
+      const Trees = await fetchArbreRemarquableParis(rows,start);
+     // TODO write a function to filter  only the data we need : ibase,libbellfrancais,genre,espee,hauteur, circonference,adresse,date planation
+
+
+      if (Trees.length !== 0) {
+        res.json(Trees);
+      } else {
+        res.json({ message: `page could not be found` });
+      }
+    } else {
+      res.status(400);
+      res.json({ message: "next param is missing" });
+    }
+  } catch (error) {
+    res.status(500);
+    res.json({ message: "Something went wrong", error });
+  }
 }
