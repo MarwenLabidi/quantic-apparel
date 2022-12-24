ab6e27ac3ed5abe26b53dd835679033bb39899c3
MarwenLabidi <marwenlabidi.dev@gmail.com>
2022-12-24 10:02:53 +0100

    🎉 i finish working on  GetLesArbresRemarquablesData serveless function🎉

--- pages/api/GetLesArbresRemarquablesData.js
+++ pages/api/GetLesArbresRemarquablesData.js
@@ -1,30 +1,34 @@
-
 //NOTE : use start query parameter to get the next page of data (pagination) 10 items per page
 // http://localhost:3000/api/GetLesArbresRemarquablesData?rows=10&start=1
 
 import { fetchArbreRemarquableParis } from "../../lib/fetchTreeDataTenItemPerPage";
-
+import { getOnlyNiceSpesificAtt } from "../../lib/filterData";
 
 export default async function handler(req, res) {
-  const {rows, start } = req.query;
+        const { rows, start } = req.query;
 
-  try {
-    if (rows) {
-      const Trees = await fetchArbreRemarquableParis(rows,start);
-     // TODO write a function to filter  only the data we need : ibase,libbellfrancais,genre,espee,hauteur, circonference,adresse,date planation
+        try {
+                if (rows) {
+                        const Trees = await fetchArbreRemarquableParis(
+                                rows,
+                                start
+                        );
+                        // TODO write a function to filter  only the data we need : ibase,libbellfrancais,genre,espee,hauteur, circonference,adresse,date planation
+                        const filtredAtt = getOnlyNiceSpesificAtt(Trees);
 
-
-      if (Trees.length !== 0) {
-        res.json(Trees);
-      } else {
-        res.json({ message: `page could not be found` });
-      }
-    } else {
-      res.status(400);
-      res.json({ message: "next param is missing" });
-    }
-  } catch (error) {
-    res.status(500);
-    res.json({ message: "Something went wrong", error });
-  }
+                        if (Trees.length !== 0) {
+                                res.json(filtredAtt);
+                        } else {
+                                res.json({
+                                        message: `page could not be found`,
+                                });
+                        }
+                } else {
+                        res.status(400);
+                        res.json({ message: "next param is missing" });
+                }
+        } catch (error) {
+                res.status(500);
+                res.json({ message: "Something went wrong", error });
+        }
 }
