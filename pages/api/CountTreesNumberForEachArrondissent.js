4a085d4ba647ad3f6e761e73043f1b865d458c56
MarwenLabidi <marwenlabidi.dev@gmail.com>
2022-12-24 08:32:13 +0100

    🚧 Work in progress : i start working on the Serverless functions to get the data from the API they are 5 function you can find them in the pages/api location

---
+++ pages/api/CountTreesNumberForEachArrondissent.js
@@ -0,0 +1,6 @@
+
+//NOTE : there is 20 arrondissements in Paris so loop through the data and count the number of trees for each arrondissement
+export default function handler(req, res) {
+	res.status(200).json({ name: 'CountTreesNumberForEachArrondissent' })
+      }
+      
---
+++ pages/api/GetLesArbresRemarquablesData.js
@@ -0,0 +1,5 @@
+
+//NOTE : use start query parameter to get the next page of data (pagination) 10 items per page
+export default function handler(req, res) {
+  res.status(200).json({ name: 'GetLesArbresRemarquablesData' })
+}
