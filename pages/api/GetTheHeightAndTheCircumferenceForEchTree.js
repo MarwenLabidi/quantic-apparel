4a7f09ec27f721c7d2d067c1a27940c6dfdf1f33
MarwenLabidi <marwenlabidi.dev@gmail.com>
2022-12-24 11:28:28 +0100

    🎉 i finish working on GetTheHeightAndTheCircumferenceForEchTree serverless function 🎉

--- pages/api/GetTheHeightAndTheCircumferenceForEchTree.js
+++ pages/api/GetTheHeightAndTheCircumferenceForEchTree.js
@@ -1,5 +1,20 @@
-export default function handler(req, res) {
-        res.status(200).json({
-                name: "GetTheHeightAndTheCircumferenceForEchTree",
-        });
+import { fetchArbreRemarquableParis } from "../../lib/fetchTreeDataTenItemPerPage";
+import { getHeightCirconferenceen } from "../../lib/filterData";
+
+export default async function handler(req, res) {
+        try {
+                const Trees = await fetchArbreRemarquableParis();
+                const filtredAtt = getHeightCirconferenceen(Trees);
+
+                if (Trees.length !== 0) {
+                        res.json(filtredAtt);
+                } else {
+                        res.json({
+                                message: `page could not be found`,
+                        });
+                }
+        } catch (error) {
+                res.status(500);
+                res.json({ message: "Something went wrong", error });
+        }
 }
