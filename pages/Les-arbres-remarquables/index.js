7bf3331e4125a3e8991ff5f4a0ffbfd9a789a4f1
MarwenLabidi <marwenlabidi.dev@gmail.com>
2022-12-24 23:16:38 +0100

    🚧 Work in progress : i start  working on les arbres remarquables page

--- pages/Les-arbres-remarquables/index.js
+++ pages/Les-arbres-remarquables/index.js
@@ -1,5 +1,10 @@
 import {LesArbresRemarquables}from"./index.module.css"
+import useSWR from "swr";
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+const URL="http://localhost:3000/api/GetLesArbresRemarquablesData?rows=10&start=1"
 const LesArbresRemarquable = () => {
+  const { data, error, isLoading } = useSWR(URL, fetcher);
+
   return (
     <div className={LesArbresRemarquables}>LesArbresRemarquable</div>
   )
