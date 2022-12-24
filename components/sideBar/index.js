3761b17262706fb4574ce7fe46a8c55d732318d7
MarwenLabidi <marwenlabidi.dev@gmail.com>
2022-12-24 14:07:42 +0100

    💄 Updating style files : i updated the sidebar components styles

--- components/sideBar/index.js
+++ components/sideBar/index.js
@@ -1,12 +1,29 @@
-import {sideBar,logo,menu,overview}from "./index.module.css"
+import {sideBar,logo,menu,overview,pages,charts,map,button,title}from "./index.module.css"
 import Image from 'next/image'
 
 
 const SideBar = () => {
         return <div className={sideBar}>
-                <Image className={logo} src="/LOGO.png" alt="Picture of the author" width={255} height={50} />
+                <Image className={logo} src="/LOGO.png" alt="Picture of the author" width={255} height={40} />
                 <main className={menu}>
                         <section  className={overview} >
+                                <p className={title}>Menu</p>
+                                <button className={button}>Overview</button>
+                        </section>
+                        <section  className={pages} >
+                                <p className={title}>Pages</p>
+                                <button className={button}>Remarkable trees</button>
+                        </section>
+                        <section  className={charts} >
+                                <p className={title}>Charts</p>
+                                <button className={button}>Bar Graph</button>
+                                <button className={button}>Pie Chart</button>
+                                <button className={button}>Scatterplot</button>
+
+                        </section>
+                        <section  className={map} >
+                                <p className={title}>Map</p>
+                                <button className={button}>Map</button>
                         </section>
                 </main>
         </div>
