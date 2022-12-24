d3ed945b47a784b4bb24bf2497be1c39f075c7b7
MarwenLabidi <marwenlabidi.dev@gmail.com>
2022-12-24 11:05:24 +0100

    🎉 i finish working on countTreesNumberForEachArrondisment serverless function 🎉

--- pages/api/CountTreesNumberForEachArrondissent.js
+++ pages/api/CountTreesNumberForEachArrondissent.js
@@ -2,19 +2,16 @@
 //NOTE : write a function to filter all the trees by arrondissement(arrays of ech arondisment have his trees) and count the number(percentage) of trees for each arrondissement
 
 import { fetchArbreRemarquableParis } from "../../lib/fetchTreeDataTenItemPerPage";
-import { getArrondissement,deleteDuplicates } from "../../lib/filterData";
+import { countTreesByArrondissement } from "../../lib/filterData";
 
 export default async function handler(req, res) {
         try {
                 const Trees = await fetchArbreRemarquableParis();
-                //write a function to get all arrondissements from the data
-		const arrondissements = getArrondissement(Trees);
-                // write a function to delete duplicate value
-		const arrondissementsWithoutDuplicates = deleteDuplicates(arrondissements);
                 // write a function to count the number of trees for each arrondissement
+		const treesCount=countTreesByArrondissement(Trees);
 
                 if (Trees.length !== 0) {
-                        res.json(arrondissementsWithoutDuplicates);
+                        res.json(treesCount);
                 } else {
                         res.json({
                                 message: `page could not be found`,
