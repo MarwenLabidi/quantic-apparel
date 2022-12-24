79f17d84c9bc88fc648eb6b7d434a4043c9b4142
MarwenLabidi <marwenlabidi.dev@gmail.com>
2022-12-24 13:22:58 +0100

    🚧 Work in progress : im working on the sideBar component

--- components/sideBar/index.js
+++ components/sideBar/index.js
@@ -1,7 +1,15 @@
-import {sideBar}from "./index.module.css"
+import {sideBar,logo,menu,overview}from "./index.module.css"
+import Image from 'next/image'
+
 
 const SideBar = () => {
-        return <div className={sideBar}>SideBar</div>;
+        return <div className={sideBar}>
+                <Image className={logo} src="/LOGO.png" alt="Picture of the author" width={255} height={50} />
+                <main className={menu}>
+                        <section  className={overview} >
+                        </section>
+                </main>
+        </div>
 };
 
 export default SideBar;
