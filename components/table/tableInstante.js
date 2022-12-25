b8615eff797c5c2490f057cea4305b8351155846
MarwenLabidi <marwenlabidi.dev@gmail.com>
2022-12-25 09:05:36 +0100

    💄 Updating style files : i updated the table component styles

--- components/table/tableInstante.js
+++ components/table/tableInstante.js
@@ -1,28 +1,28 @@
 import React from 'react'
 import {useTable} from 'react-table'
 import {COLUMNN} from './column'
-
+import {table,td,th,tr} from './table.module.css'
 const TableInstante = ({data}) => {
   const columns = React.useMemo(() => COLUMNN, [])
   const datas = React.useMemo(() => data, [])
   const tableInstance = useTable({columns, data:datas})
   const {getTableProps, getTableBodyProps, headerGroups, rows, prepareRow} = tableInstance
   return (
-    <table {...getTableProps()}>
-      <thead>
+    <table className={table} {...getTableProps()}>
+      <thead className={th}>
         {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
+          <tr className={tr} {...headerGroup.getHeaderGroupProps()}>
             {headerGroup.headers.map((column) => (
               <th {...column.getHeaderProps()}>{column.render('Header')}</th>
             ))}
           </tr>
         ))}
       </thead>
-      <tbody {...getTableBodyProps()}>
+      <tbody className={td} {...getTableBodyProps()}>
         {rows.map((row) => {
           prepareRow(row)
           return (
-            <tr {...row.getRowProps()}>
+            <tr className={tr} {...row.getRowProps()}>
               {row.cells.map((cell) => {
                 return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
               })}
