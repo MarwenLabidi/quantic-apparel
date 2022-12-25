b00f0d79972a9c2a98d6ccc427f78fe5200ed508
MarwenLabidi <marwenlabidi.dev@gmail.com>
2022-12-25 08:45:26 +0100

    🚧 Work in progress : i start  working on the table component

--- components/table/tableInstante.js
+++ components/table/tableInstante.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import {useTable} from 'react-table'
+import {COLUMNN} from './column'
 
-const tableInstante = () => {
+const TableInstante = ({data}) => {
+  const columns = React.useMemo(() => COLUMNN, [])
+  const datas = React.useMemo(() => data, [])
+  const tableInstance = useTable({columns, data:datas})
+  const {getTableProps, getTableBodyProps, headerGroups, rows, prepareRow} = tableInstance
   return (
-    <div>tableInstante</div>
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row)
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+              })}
+            </tr>
+          )
+        }
+        )}
+      </tbody>
+    </table>
   )
 }
 
-export default tableInstante
+export default TableInstante
--- pages/Les-arbres-remarquables/index.js
+++ pages/Les-arbres-remarquables/index.js
@@ -2,11 +2,18 @@ import {LesArbresRemarquables}from"./index.module.css"
 import useSWR from "swr";
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 const URL="http://localhost:3000/api/GetLesArbresRemarquablesData?rows=10&start=1"
+import TableInstante from "../../components/table/tableInstante";
 const LesArbresRemarquable = () => {
   const { data, error, isLoading } = useSWR(URL, fetcher);
 
   return (
-    <div className={LesArbresRemarquables}>LesArbresRemarquable</div>
+    <div className={LesArbresRemarquables}>
+      <h1>Les arbres remarquables</h1>
+      {isLoading && <div>Loading...</div>}
+      {error && <div>Error: {error.message}</div>}
+      {data && (<TableInstante data={data} />)}
+
+    </div>
   )
 }
 
