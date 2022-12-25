a06226c866661893b9f8aabea2d976850e94d3b7
MarwenLabidi <marwenlabidi.dev@gmail.com>
2022-12-25 09:09:15 +0100

    💄 Updating style files : i updated the table component styles

--- pages/Les-arbres-remarquables/index.js
+++ pages/Les-arbres-remarquables/index.js
@@ -1,7 +1,7 @@
 import {LesArbresRemarquables}from"./index.module.css"
 import useSWR from "swr";
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
-const URL="http://localhost:3000/api/GetLesArbresRemarquablesData?rows=5&start=1"
+const URL="http://localhost:3000/api/GetLesArbresRemarquablesData?rows=6&start=1"
 import TableInstante from "../../components/table/tableInstante";
 const LesArbresRemarquable = () => {
   const { data, error, isLoading } = useSWR(URL, fetcher);
