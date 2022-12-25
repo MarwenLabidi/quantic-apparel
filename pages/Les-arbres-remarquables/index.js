9209fe2898dbd3f1b58c880bc6764033fbc1d492
MarwenLabidi <marwenlabidi.dev@gmail.com>
2022-12-25 09:31:05 +0100

    🚧 Work in progress : im working on the sorting feature of the table component

--- pages/Les-arbres-remarquables/index.js
+++ pages/Les-arbres-remarquables/index.js
@@ -1,8 +1,8 @@
 import {LesArbresRemarquables}from"./index.module.css"
 import useSWR from "swr";
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
-const URL="http://localhost:3000/api/GetLesArbresRemarquablesData?rows=6&start=1"
-import TableInstante from "../../components/table/tableInstante";
+const URL="http://localhost:3000/api/GetLesArbresRemarquablesData?rows=5&start=1"
+import SortingTable from "../../components/table/sortingTable";
 const LesArbresRemarquable = () => {
   const { data, error, isLoading } = useSWR(URL, fetcher);
 
@@ -11,7 +11,7 @@ const LesArbresRemarquable = () => {
       <h1>Les arbres remarquables</h1>
       {isLoading && <div>Loading...</div>}
       {error && <div>Error: {error.message}</div>}
-      {data && (<TableInstante data={data} />)}
+      {data && (<SortingTable data={data} />)}
 
     </div>
   )
