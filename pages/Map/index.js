8519bad8c4cdd7807a5d4d0c3241d86b6edf3b07
MarwenLabidi <marwenlabidi.dev@gmail.com>
2022-12-26 10:04:44 +0100

    🚧 Work in progress : i start working on the map page

--- pages/Map/index.js
+++ pages/Map/index.js
@@ -1,4 +1,46 @@
 import {map}from "./index.module.css";
+import {
+  MapContainer,
+  TileLayer,
+  Popup,
+  Marker,
+  useMapEvent,
+} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import icon from "/images/icon-location.png";
+import { useMap } from "react-leaflet";
+
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  iconSize: [46, 56],
+  // iconAnchor: [23, 56],
+  // popupAnchor: [0, -56],
+});
+function MultipleMarkers({ data }) {
+  if (!data) {
+          return;
+  }
+  return data?.map((tree, index) => {
+          if (!tree.coordinates) {
+                  return;
+          }
+          return (
+                  <Marker
+                          key={index}
+                          position={[
+                                  tree.coordinates[0],
+                                  tree.coordinates[1],
+                          ]}
+                          icon={DefaultIcon}>
+                          <Popup>
+                            <p>genre:{tree.genre}</p>
+                            <p>libellefrancais:{tree.libellefrancais}</p>
+                          </Popup>
+                  </Marker>
+          );
+  });
+}
 const Map = () => {
   return (
     <div className={map}>Map</div>
